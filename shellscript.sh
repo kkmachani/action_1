4eff15dffbef14cc16d82d6d5a8e0a01d30e8fdd
kkmachani <118263597+kkmachani@users.noreply.github.com>
2024-09-05 15:55:22 +0530

    shellscript commit

---
+++ shellscript.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+echo "This is My first shellsript in sample workflow" > a.txt
+echo "This is My first shellsript in sample workflow" > b.txt
+echo "This is My first shellsript in sample workflow" > c.txt
+
+ls -ltra 
+
+cat a.txt b.txt c.txt
+
+grep 'workflow' ./*
+
+sed -i 's/workflow/workflowjob/g' *
+
+cat a.txt b.txt c.txt
+
+
+
+
